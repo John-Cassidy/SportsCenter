a235ec625ca6b7114e2f221780fc1025183b73cb
John Cassidy <jpcassidy@outlook.com>
2024-04-04 11:16:14 -0400

    implement loading products from api on app component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { CoreComponent } from './core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/product';
 import { RouterOutlet } from '@angular/router';
 import { SharedComponent } from './shared';
 
@@ -11,6 +14,19 @@ import { SharedComponent } from './shared';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Sports Center';
+  products: Product[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get<Product[]>(
+        'http://localhost:5096/products?Sort=NameAsc&Skip=0&Take=10'
+      )
+      .subscribe((data) => {
+        this.products = data;
+      });
+  }
 }
