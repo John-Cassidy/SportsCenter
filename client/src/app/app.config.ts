39923dea40c148b927435654acdd94566708ede6
John Cassidy <jpcassidy@outlook.com>
2024-04-04 10:31:34 -0400

    Add HttpClient provider to appConfig

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,7 +1,8 @@
 import { APP_ROUTES } from './app.routes';
 import { ApplicationConfig } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(APP_ROUTES)],
+  providers: [provideRouter(APP_ROUTES), provideHttpClient()],
 };
