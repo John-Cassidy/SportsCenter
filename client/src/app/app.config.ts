8b89c148f8d4880796fb17b699391b7ee00aeae3
John Cassidy <jpcassidy@outlook.com>
2024-04-06 09:55:08 -0400

    add hash based routing to router configuration in app.config.ts

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,13 +1,13 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { APP_ROUTES } from './app.routes';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { provideHttpClient } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(APP_ROUTES),
+    provideRouter(APP_ROUTES, withHashLocation()),
     provideHttpClient(),
     importProvidersFrom(PaginationModule.forRoot()),
   ],
