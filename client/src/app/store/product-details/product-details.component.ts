1d42803324bbc6051186a8f06a844c6392c7159d
John Cassidy <jpcassidy@outlook.com>
2024-04-06 09:33:08 -0400

    Refactor product details component

--- client/src/app/store/product-details/product-details.component.ts
+++ client/src/app/store/product-details/product-details.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ActivatedRoute } from '@angular/router';
+import { CoreComponent } from '../../core';
+import { Product } from '../../shared/models/Product';
+import { SharedComponent } from '../../shared';
+import { StoreService } from '../store.service';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [],
+  imports: [CoreComponent, SharedComponent],
   templateUrl: './product-details.component.html',
-  styleUrl: './product-details.component.scss'
+  styleUrl: './product-details.component.scss',
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  product?: Product;
+  quantity: number = 1;
 
+  constructor(
+    private storeService: StoreService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.loadProduct();
+  }
+
+  loadProduct() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    if (id) {
+      this.storeService.getProduct(+id).subscribe({
+        next: (product) => {
+          this.product = product;
+        },
+        error: (error) => console.log(error),
+      });
+    }
+  }
+
+  addToCart() {}
+
+  // Method to increment the quantity
+  incrementQuantity() {
+    this.quantity++;
+  }
+
+  // Method to decrement the quantity (prevent negative values)
+  decrementQuantity() {
+    if (this.quantity > 1) {
+      this.quantity--;
+    }
+  }
 }
